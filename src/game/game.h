9655a28e1893950a2494dcb186cf9dad9c08c298
Gui Lin <guileen@gmail.com>
2018-09-27 11:54:17 +0800

    sperate folders

---
+++ src/game/game.h
@@ -0,0 +1,21 @@
+//
+//  game.h
+//  endlessworld
+//
+//  Created by 林 桂 on 18/9/27.
+//  Copyright © 2018年 林 桂. All rights reserved.
+//
+
+#ifndef game_h
+#define game_h
+
+class GameScean {
+    // vector<Sprite>
+    
+public:
+    handleEvent();
+    updateFrame();
+    render();
+};
+
+#endif /* game_h */
--- src/scean.c
+++
@@ -1,9 +0,0 @@
-//
-//  scean.c
-//  endlessworld
-//
-//  Created by 林 桂 on 18/9/23.
-//  Copyright © 2018年 林 桂. All rights reserved.
-//
-
-#include "scean.h"
