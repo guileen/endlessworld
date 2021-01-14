2938effb96622ec06cb7aeaa89c010261e1e9eaf
Gui Lin <guileen@gmail.com>
2021-01-14 22:51:53 +0800

    update legacy code

--- src/commons/sdl2utils.h
+++ src/commons/sdl2utils.h
@@ -31,9 +31,9 @@
 #ifdef __APPLE__
 
 #include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
-#include <SDL2_ttf/SDL_ttf.h>
-#include <SDL2_mixer/SDL_mixer.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 #define FONT_DEFAULT "/Library/Fonts/Arial.ttf"
 #else
 
