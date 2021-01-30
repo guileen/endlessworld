0094f086c51b136900e84c93e5de890aa0257f71
Gui Lin <guileen@gmail.com>
2021-01-31 07:44:19 +0800

    add insight asset loader

--- apps/texture-demo.cpp
+++ apps/texture-demo.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <insight/assets.hpp>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
@@ -94,64 +96,16 @@ int main()
     // load and create a texture 
     // -------------------------
     unsigned int texture1, texture2;
-    // texture 1
-    // ---------
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1); 
-     // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-    int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char *data = stbi_load(filesystem::path("resources/textures/container.jpg").c_str(), &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-    // texture 2
-    // ---------
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-    data = stbi_load(filesystem::path("resources/textures/awesomeface.png").c_str(), &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
+    // 使用 insight 加载素材
+    ins::loadTexture("resources/textures/container.jpg", &texture1, ins::rgb);
+    ins::loadTexture("resources/textures/awesomeface.png", &texture2, ins::rgba);
 
     // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
     // -------------------------------------------------------------------------------------------
     ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
-    // either set it manually like so:
-    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
-    // or set it via the texture class
-    ourShader.setInt("texture2", 1);
-
-
+    // 使用 insight 绑定素材
+    ins::uniformTexture(ourShader.ID, "texture1", GL_TEXTURE0, texture1);
+    ins::uniformTexture(ourShader.ID, "texture2", GL_TEXTURE1, texture2);
 
     // render loop
     // -----------
@@ -165,12 +119,6 @@ int main()
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // bind textures on corresponding texture units
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
 
         // render container
         ourShader.use();
--- include/insight/assets.hpp
+++ include/insight/assets.hpp
@@ -4,19 +4,31 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 
-#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#include <filesystem>
+#include <iostream>
 
-namespace insight
+namespace ins
 {
+    enum
+    {
+        default_channels = 0, // only used for desired_channels
+        grey = 1,
+        greya = 2,
+        rgb = 3,
+        rgba = 4
+    };
     int loadImage(const char *path);
     int load3DModel(const char *path);
-    int loadTexture(const char *path);
+    // 加载材质
+    int loadTexture(const char *path, unsigned int *texture, int req_channels);
+    int uniformTexture(int shaderId, const char *glVariableName, int glTextureID, int texture);
+
 }; // namespace imm
 
-namespace insight
+namespace ins
 {
-    int loadTexture(const char *path, unsigned int* texture)
+    int loadTexture(const char *path, unsigned int* texture, int req)
     {
         glGenTextures(1, texture);
         glBindTexture(GL_TEXTURE_2D, *texture);
@@ -27,10 +39,12 @@ namespace insight
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         // 加载并生成纹理
         int width, height, nrChannels;
-        unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+        stbi_set_flip_vertically_on_load(true);
+        unsigned char *data = stbi_load(std::filesystem::path(path).c_str(), &width, &height, &nrChannels, req);
         if (data)
         {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            int glFlag = req == STBI_rgb_alpha ? GL_RGBA: GL_RGB;
+            glTexImage2D(GL_TEXTURE_2D, 0, glFlag, width, height, 0, glFlag, GL_UNSIGNED_BYTE, data);
             glGenerateMipmap(GL_TEXTURE_2D);
         }
         else
@@ -39,6 +53,18 @@ namespace insight
             return 1;
         }
         stbi_image_free(data);
+        return 0;
+    }
+
+    int uniformTexture(int shaderId, const char *glVariableName, int glTextureID, int texture) {
+        if (glTextureID < GL_TEXTURE0 || glTextureID > GL_TEXTURE31) {
+            std::cout << "uniformTexture: Invalid glTextureID" << glTextureID << std::endl;
+            return 1;
+        }
+        int location = glTextureID - GL_TEXTURE0;
+        glUniform1i(glGetUniformLocation(shaderId, glVariableName), location);
+        glActiveTexture(glTextureID);
+        glBindTexture(GL_TEXTURE_2D, texture);
         return 0;
     }
 } // namespace immersion
